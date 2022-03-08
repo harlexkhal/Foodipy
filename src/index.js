6c7c2e72757fc3580df33f532838ef443f303138
Mnemba Chambuya <karimnembac@gmail.com>
2022-03-08 12:54:50 +0300

    add header and main gui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 import Icon from './assets/images/loading.gif';
 import Application from '../custom_modules/application/application.js';
+import '@fortawesome/fontawesome';
 
 const loader = document.querySelector('.loading');
 loader.src = Icon;
