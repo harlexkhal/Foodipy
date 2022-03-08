13d8205b0c8da2ad3fe81db4df26b171a447bc55
Alexander Oguzie-Ibeh <harlexibeh01@gmail.com>
2022-03-08 16:54:19 +0100

    fix forseen merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import './index.css';
-import Icon from './assets/images/loading.gif';
+import '@fortawesome/fontawesome-free/js/all.js';
 import ApplicationEngine from '../custom_modules/application/application.js';
-
-const loader = document.querySelector('.loading');
-loader.src = Icon;
 
 const application = new ApplicationEngine();
 application.start();
+
+const modal = document.querySelector('#the-modal');
+
+const modalCloser = document.querySelector('#modal-closer');
+modalCloser.addEventListener('click', () => {
+  modal.classList.remove('d-block');
+  modal.classList.add('d-none');
+});
+
+let commentSpans = document.querySelectorAll('.comments');
+commentSpans = Array.from(commentSpans);
+commentSpans.forEach((span) => {
+  span.addEventListener('click', () => {
+    modal.classList.remove('d-none');
+    modal.classList.add('d-block');
+  });
+});
