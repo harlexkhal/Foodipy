92538a79fbf74c6ac4bf60cdf5884c688c64f82f
Mnemba Chambuya <karimnembac@gmail.com>
2022-03-08 13:45:42 +0300

    add footer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 import Icon from './assets/images/loading.gif';
 import Application from '../custom_modules/application/application.js';
-import '@fortawesome/fontawesome';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 const loader = document.querySelector('.loading');
 loader.src = Icon;
