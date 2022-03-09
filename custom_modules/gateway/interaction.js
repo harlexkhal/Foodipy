b340418ec97c62c87dd0cadccfd2f79ccfd815a5
Alexander Oguzie-Ibeh <harlexibeh01@gmail.com>
2022-03-09 07:47:48 +0100

    add index link to item count + initialize involvement api

--- custom_modules/gateway/interaction.js
+++ custom_modules/gateway/interaction.js
@@ -1,6 +1,10 @@
 import NetworkGateway from './gateway.js';
 
 class InteractionGateway extends NetworkGateway {
+    constructor(instanceID) {
+        super('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/');
+        this.dbInstance = instanceID;
+    }
     init = () => {}
 }
 export default InteractionGateway;
